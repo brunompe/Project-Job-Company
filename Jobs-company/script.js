d8656c0153078938df4b329a64a5d286c7361f5a
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-28 15:09:22 -0400

    script.js e style.css

--- Jobs-company/script.js
+++ Jobs-company/script.js
@@ -1,6 +1,6 @@
 const URL_ROOT = 'https://api.adzuna.com/v1/api';
 const APPLICATION_ID = '3fabcb1c';
-const APPLICATION_KEYS = '268aee69acbf19580edb1fffc3c06fbb';
+const APPLICATION_KEYS = '2bfd5e90cb19ade4d6989af016090452';
 
 const form_header_option = document.querySelector('.btn-submit');
 const input_job = document.querySelector('.input-job');
@@ -23,16 +23,16 @@ function addEventOnButtons() {
 addEventOnButtons();
 
 function makeRequest(param1, param2) {
-  fetch(`http://api.adzuna.com/v1/api/jobs/${param2}/search/1?app_id=3fabcb1c&app_key=268aee69acbf19580edb1fffc3c06fbb&results_per_page=50&what=${param1}&content-type=application/json`)
+  fetch(`http://api.adzuna.com/v1/api/jobs/${param2}/search/1?app_id=${APPLICATION_ID}_key=${APPLICATION_KEYS}&results_per_page=50&what=${param1}&content-type=application/json`)
     .then(r => r.json())
     .then(response => response.results)
-    // .then(results => console.log(results))
-    .then(results => results.forEach((result) => {
-      const paragraph = document.createElement('p');
-      paragraph.className = 'names';
-      paragraph.innerText = result.title;
-      div_results.appendChild(paragraph);
-    }))
+    .then(results => console.log(results))
+    // .then(results => results.forEach((result) => {
+    //   const paragraph = document.createElement('p');
+    //   paragraph.className = 'names';
+    //   paragraph.innerText = result.title;
+    //   div_results.appendChild(paragraph);
+    // }))
 }
 
 window.onload = async () => {
