7000e41ebb4e2c2c7ee23bd35736056d688e4665
lucas-morais <lucasmoraisss@gmail.com>
2021-09-28 17:57:06 -0300

    limpando arquivos

--- Jobs-company/script.js
+++ Jobs-company/script.js
@@ -1,15 +1,1 @@
-function getData() {
-  const API_KEY = 'Hwo8Lqnu2EDBVXyyMJz4whuSDfCPPglwMRIoxqsD+DM=';
-  const request = {
-    url: 'data.usajobs.gov',
-    method: 'GET',
-    headers: {
-      'Host': 'data.usajobs.gov',
-      'User-Agent': 'brunompe@gmail.com',
-      'Authorization-Key': API_KEY,
-    }
-  }
 
-  const response = fetch('https://data.usajobs.gov/api/search');
-  console.log(response);
-}
